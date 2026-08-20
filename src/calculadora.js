358e98ebd2929950fa2775b867f7444dc0aed989
Valentina Bernardes Soares <valentina.bsoares@dinfor.uniceub>
2026-08-20 09:03:18 -0300

    test: corrige linter e quebra regra de negocio no teste

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,7 +1,7 @@
 function somar(a, b) {
-  return a + b;
+ return a - b; // Erro de lógica proposital
 }
-const x = 100; // Variável sem uso para forçar erro no ESLint
+// const x = 100; // Variável sem uso para forçar erro no ESLint
 function subtrair(a, b) {
   return a - b;
 }
