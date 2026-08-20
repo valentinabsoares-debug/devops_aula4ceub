dbc4ec5fc0ad77d829669b4c5168d8adf202ed06
valentinabsoares-debug <valentina.bsoares@sempreceub.com>
2026-08-20 09:07:18 -0300

    Update calculadora.js

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,5 +1,5 @@
 function somar(a, b) {
- return a - b; // Erro de lógica proposital
+ return a + b; // Erro de lógica proposital
 }
 // const x = 100; // Variável sem uso para forçar erro no ESLint
 function subtrair(a, b) {
